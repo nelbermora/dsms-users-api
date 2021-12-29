44b1d46f0cad48b1ecb4d893d664929b337676ca
A308071 <nmoramora@santandertecnologia.com.ar>
2021-12-29 01:50:24 -0300

    Users crud from cached data

--- main/main.go
+++ main/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	router, _ := server.SetupDependencies()
 	log.Println("initializing Users API")
-	err := http.ListenAndServe(":18080", router)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("could not initialize server: %s", err.Error()))
 	}
